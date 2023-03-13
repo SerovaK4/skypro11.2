81bf0d3cce6b6b6b00e1a13e28176df786bb9cc6
SerovaK4 <ksusha1354@gmail.com>
2023-03-13 14:23:43 +0300

    add title_funcion

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
-def str_func(user_string):
+def str_func_upper(user_string):
     return user_string.upper()
 
 
+# Функция делает заглавными первые буквы каждого слова в строке
+def str_func_title(user_string):
+    return user_string.title()
